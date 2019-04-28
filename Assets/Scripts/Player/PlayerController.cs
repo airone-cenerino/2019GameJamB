e8c048925a27b1068d71a1c1f84fb446a37ad5b7
nysyo <tds24004@gmail.com>
2019-04-28 15:47:37 +0900

    [Add]プレイヤーの作成

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Camera m_camera;
+    Vector3 center;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_camera = Camera.main;
+        center = new Vector3(Screen.width / 2, Screen.height / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 画面の中心にレイを飛ばす
+            Ray ray = m_camera.ScreenPointToRay(center);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, 1000f))
+            {
+                // アイテムに対する処理を書く
+            }
+
+        }
     }
 }
