61212c8561e5819524425ffa256cb4b3bf9c8694
nysyo <tds24004@gmail.com>
2019-04-28 18:08:35 +0900

    プレイヤーの調整

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    private Camera m_camera;
-    Vector3 center;
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        m_camera = Camera.main;
-        center = new Vector3(Screen.width / 2, Screen.height / 2);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        private Camera m_camera;
+        Vector3 center;
+        // Start is called before the first frame update
+        void Start()
         {
-            // 画面の中心にレイを飛ばす
-            Ray ray = m_camera.ScreenPointToRay(center);
-            RaycastHit hit;
-            if(Physics.Raycast(ray,out hit, 1000f))
-            {
-                // アイテムに対する処理を書く
-            }
+            m_camera = Camera.main;
+            center = new Vector3(Screen.width / 2, Screen.height / 2);
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // 画面の中心にレイを飛ばす
+                Ray ray = m_camera.ScreenPointToRay(center);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100f))
+                {
+                    // アイテムに対する処理を書く
+                    hit.collider.gameObject.SendMessage("Use");
+                }
+            }
         }
     }
 }
