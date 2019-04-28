5cdf865505dc76fce66b68eb2ebac9f811bb5877
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-04-28 23:31:15 +0900

    恐怖BGM実装完了

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
+// Enemyにアタッチするスクリプト
+
 namespace Enemy
 {
     enum EnemyCondition
@@ -20,17 +22,23 @@ namespace Enemy
         private GameObject player;
         private NavMeshAgent navMeshAgent;
 
+        [SerializeField] private AudioSource horrorBGMAudioSource;
         [SerializeField] private GameObject rightFootPrint;
         [SerializeField] private GameObject leftFootPrint;
         [SerializeField] private List<AudioClip> audioClips;
+        [SerializeField] private AudioClip horrorBGM;
 
 
-        private float walkedDistance = 0.0f;   // 歩いた距離
-        private float stepLength = 1f;
-        private Vector3 lastPosition;
-        private bool flg = false;
+        private float walkedDistance = 0.0f;    // 歩いた距離
+        private float stepLength = 1f;          // 1歩の長さ
+        private Vector3 lastPosition;           // 1フレーム前の場所
+        private bool flg = false;               // 右か左かの確認
+        private float horrorSoundStartDistance = 8f;   // 恐怖BGMが流れる距離
+        private float horrorSoundMaxVolume = 0.2f;
+        private float horrorSoundVolume = 0.2f;
+        private bool IsInHorrorSoundArea = false;
 
-        EnemyCondition state;   // Enemyのステート
+        EnemyCondition state;                   // Enemyのステート
 
         // Start is called before the first frame update
         void Start()
@@ -40,6 +48,7 @@ namespace Enemy
             navMeshAgent = GetComponent<NavMeshAgent>();
             player = GameObject.FindGameObjectWithTag("Player");
 
+            horrorBGMAudioSource.clip = horrorBGM;
             state = EnemyCondition.Chase;
             lastPosition = transform.position;
         }
@@ -47,38 +56,17 @@ namespace Enemy
         // Update is called once per frame
         void Update()
         {
-            // 動いた距離計算
-            walkedDistance += Vector3.Distance(transform.position, lastPosition);
-            if (walkedDistance > stepLength)
-            {
-                audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Count())]);
-
-                Vector3 instantiatePosition = new Vector3(transform.position.x, transform.position.y - capsuleCollider.height / 2f - 0.05f, transform.position.z);
-
-                if (flg)
-                { 
-                    GameObject footprint = Instantiate(rightFootPrint, instantiatePosition, transform.rotation);
-                    footprint.transform.position += footprint.transform.right * 0.2f;
-                    flg = false;
-                }
-                else
-                {
-                    GameObject footprint = Instantiate(leftFootPrint, instantiatePosition, transform.rotation);
-                    footprint.transform.position += footprint.transform.right * -0.2f;
-                    flg = true;
-                }
-                walkedDistance = 0.0f;
-            }
-            
+             // ステート管理
             if(state == EnemyCondition.Chase)
             {
                 navMeshAgent.destination = player.transform.position;   // エージェントの目的地をPlayerの現在地に更新
-            }else if(state == EnemyCondition.Stop)
+                FootPrintGenerate();    // 足跡の処理
+                HorrorBGMControl();     // 恐怖BGMの処理
+            }
+            else if(state == EnemyCondition.Stop)
             {
                 navMeshAgent.destination = transform.position;
             }
-
-            lastPosition = transform.position;
         }
 
         public void OnTriggerEnter(Collider other)
@@ -97,6 +85,68 @@ namespace Enemy
         public void EnemyChaseStart()
         {
             state = EnemyCondition.Chase;
+        }
+
+
+        /*
+         *以下Private
+         */
+
+        // 足跡生成の処理
+        private void FootPrintGenerate()
+        {
+            // 動いた距離計算
+            walkedDistance += Vector3.Distance(transform.position, lastPosition);
+
+            if (walkedDistance > stepLength)    // 1歩の長さを超えたら
+            {
+                audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Count())]);   // 足音
+
+                // 足跡の座標
+                Vector3 instantiatePosition = new Vector3(transform.position.x, transform.position.y - capsuleCollider.height / 2f - 0.05f, transform.position.z);
+
+                if (flg)
+                {
+                    GameObject footprint = Instantiate(rightFootPrint, instantiatePosition, transform.rotation);
+                    footprint.transform.position += footprint.transform.right * 0.2f;
+                    flg = false;
+                }
+                else
+                {
+                    GameObject footprint = Instantiate(leftFootPrint, instantiatePosition, transform.rotation);
+                    footprint.transform.position += footprint.transform.right * -0.2f;
+                    flg = true;
+                }
+
+                walkedDistance = 0.0f;  // 歩いた距離初期化
+            }
+
+            lastPosition = transform.position;
+        }
+
+        // 恐怖BGMの処理
+        private void HorrorBGMControl()
+        {
+            if ((Vector3.Distance(player.transform.position, transform.position) < horrorSoundStartDistance))
+            {
+                if (!IsInHorrorSoundArea)   // 今まで外にいて、中に入った時
+                {
+                    horrorSoundVolume = horrorSoundMaxVolume;
+                    horrorBGMAudioSource.volume = horrorSoundVolume;
+                    horrorBGMAudioSource.Play();
+                    IsInHorrorSoundArea = true;
+                }
+            }
+            else
+            {   // エリア中にいないとき
+                IsInHorrorSoundArea = false;
+                horrorSoundVolume -= Time.deltaTime / 2f;
+                horrorBGMAudioSource.volume = horrorSoundVolume;
+                if (horrorSoundVolume < 0)
+                {
+                    horrorBGMAudioSource.Stop();
+                }
+            }
         }
     }
 }
--- Assets/Scripts/System/GamePauseMenu.cs
+++ Assets/Scripts/System/GamePauseMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 using Enemy;
 using UnityEngine.SceneManagement;
 
+
+// ゲーム中のポーズ画面管理
+
 namespace System
 {
     public class GamePauseMenu : MonoBehaviour
@@ -17,21 +20,23 @@ namespace System
             enemyController = GameObject.FindWithTag("Enemy").GetComponent<EnemyController>();
         }
 
-        // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 enemyController.EnemyStop();
                 pausePanel.SetActive(true);
-                //カーソル表示on
+                Cursor.visible = true;  //カーソル表示on
             }
         }
 
+
+        // 以下ボタン用関数
         public void Resume()
         {
             enemyController.EnemyChaseStart();
             pausePanel.SetActive(false);
+            Cursor.visible = false;
         }
 
         public void Quit()
