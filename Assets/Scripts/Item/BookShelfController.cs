68834f3c91efff2ce05ea2d2d2c70eb9d48c8b94
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-04-29 16:59:27 +0900

    これより本棚作成を開始する

---
+++ Assets/Scripts/Item/BookShelfController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Item
+{
+    public class BookShelfController : ItemBase
+    {
+        [SerializeField] private GameObject booksInShelf;
+        [SerializeField] private GameObject scatteredBooks;
+
+        public override void Use()
+        {
+            booksInShelf.SetActive(false);
+            scatteredBooks.SetActive(true);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
