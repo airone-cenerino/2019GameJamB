fe55dd868a9e00b3a5c9a29e045a59e92c967f12
nysyo <tds24004@gmail.com>
2019-04-29 13:32:15 +0900

    マテリアルの修正

---
+++ Assets/Scripts/Item/PCScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCScript : Item.ItemBase
+{
+    private Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Material>();
+    }
+    public override void Use()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
