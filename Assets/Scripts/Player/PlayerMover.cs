8f2f3f931ed197144eb11532231d71ca73753ff6
nysyo <tds24004@gmail.com>
2019-04-30 01:43:41 +0900

    VRに対応

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -34,8 +34,12 @@ namespace Player
             this.transform.rotation *= Quaternion.Euler(0, rotateSpeed * Input.GetAxis("Mouse X"), 0);
             m_Camera.transform.rotation *= Quaternion.Euler(-rotateSpeed * Input.GetAxis("Mouse Y"), 0, 0);
 #elif UNITY_WSA
-            if(Input.GetKeyDown("joystick button 9")){
+            if(Input.GetKeyDown("joystick button 17")){
                 UnityEngine.XR.InputTracking.Recenter();
+            }
+            if(Input.GetKey("joystick button 9"))
+            {
+                this.transform.rotation *= Quaternion.Euler(0, rotateSpeed * Input.GetAxis("Touch"), 0);
             }
 #endif
             forward = new Vector3(m_Camera.transform.forward.x, 0, m_Camera.transform.forward.z).normalized;
