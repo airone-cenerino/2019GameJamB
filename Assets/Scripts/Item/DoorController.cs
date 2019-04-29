b2f9f1128712daf00ca22edd819155cb41443117
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-04-29 13:51:00 +0900

    ドアアニメーション完了

--- Assets/Scripts/Item/DoorController.cs
+++ Assets/Scripts/Item/DoorController.cs
@@ -6,11 +6,20 @@ namespace Item
 {
     public class DoorController : ItemBase
     {
+        [SerializeField] GameObject doorCollider;
+        [SerializeField] Animator doorAnimator;
+        float time = 2.0f;
+
         public override void Use()
         {
+            doorCollider.SetActive(true);
+            doorAnimator.SetTrigger("DoorOpen");
+        }
 
-
-
+        public void DoorClose()
+        {
+            doorCollider.SetActive(false);
+            doorAnimator.SetTrigger("DoorClose");
         }
     }
 }
