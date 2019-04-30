57b9cfe4ec7d7893f7a3a0d89e39a4dc8bc1ce2d
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-04-30 11:31:16 +0900

    1週目部分完成

---
+++ Assets/Scripts/GameSystem/VanishWall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem
+{
+    public class VanishWall : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(LoopManager.loop > 1)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
