e02701a37e757c3b282c9942bc0496b10f4ffffb
Airone_Cenerino <tomopc723@yahoo.co.jp>
2019-04-30 13:58:23 +0900

    これよりLight消灯を実装する

---
+++ Assets/Scripts/Item/LightController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Item
+{
+    public class LightController : MonoBehaviour
+    {
+        LetterController letterController;
+
+        private void Start()
+        {
+            letterController = GameObject.FindGameObjectWithTag("Letter").GetComponent<LetterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
