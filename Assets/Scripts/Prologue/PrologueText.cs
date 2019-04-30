3d0982b32c1b5b5ba833a5abac8a0ee63164490f
Bright_ZZZ <elekun.8901016@gmail.com>
2019-04-30 11:31:35 +0900

    プロローグ(1回目と2回目以降)を実装しました

--- Assets/Scripts/Prologue/PrologueText.cs
+++ Assets/Scripts/Prologue/PrologueText.cs
@@ -1,90 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using GameSystem;
 
-public class PrologueText : MonoBehaviour
+
+namespace Prologue
 {
-    public GameObject text_object = null;
-    public int text_number = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PrologueText : MonoBehaviour
     {
-        
-    }
+        public GameObject text_object = null;
+        public int text_number = 0;
+        public string[] text_line1 = new string[] { "(いつから寝ていたのだろう。",
+                                                      "頭が痛い。",
+                                                      "時間は・・・？",
+                                                      "そろそろ帰らなくては。",
+                                                      "・・・・・",
+                                                      "・・・あれ？・・・スマホがない。",
+                                                      "・・・・・",
+                                                      "・・・・・・・あ、",
+                                                      "トイレに忘れたんだ。",
+                                                      "回収して帰ろう)"};
 
-    // Update is called once per frame
-    void Update()
-    {
-        Text letter = text_object.GetComponent<Text>();
+        public string[] text_line2 = new string[] { "(はっ、、、)",
+                                                      "気が付くと元の部屋に戻っていた",
+                                                      "(なんだ、夢か。",
+                                                      "・・・恐ろしい夢だった。",
+                                                      "とにかく帰ろう)",
+                                                      "急いで荷物をまとめる。",
+                                                      "(・・・・っえ、・・・)",
+                                                      "またもやスマホがない。",
+                                                      "(まさか、、、)",
+                                                      "恐怖に怯えながらも、",
+                                                      "再びあのトイレに向かうのであった。"};
 
-        if (LoopManager.loop == 1)
+        // Start is called before the first frame update
+        void Start()
         {
-            switch (text_number)
-            {
-                case 0:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 1:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 2:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 3:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 4:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                default:
-                    letter.text = "XXXXXX";
-                    break;
-            }
+
         }
 
-        if (LoopManager.loop == 2)
+        // Update is called once per frame
+        void Update()
         {
-            switch (text_number)
-            {
-                case 0:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 1:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 2:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 3:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                case 4:
-                    letter.text = "\n" +
-                                  "";
-                    break;
-                default:
-                    letter.text = "XXXXXX";
-                    break;
-            }
-        }
+            Text letter = text_object.GetComponent<Text>();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (text_number < 4) text_number++;
-            //if (text_number == 3) SceenManager.LoadScene("");
+            if (LoopManager.loop == 1 && text_number <= 7)
+                letter.text = text_line1[text_number] + "\n" + text_line1[text_number + 1] + "\n" + text_line1[text_number + 2];
+
+            if (LoopManager.loop >= 2 && text_number <= 8)
+                letter.text = text_line2[text_number] + "\n" + text_line2[text_number + 1] + "\n" + text_line2[text_number + 2];
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if ((LoopManager.loop == 1 && text_number >= 7) || (LoopManager.loop >= 2 && text_number >= 8))
+                    SceneManager.LoadScene("GameScene");
+
+                if ((LoopManager.loop == 1 && text_number < 7) || (LoopManager.loop >= 2 && text_number < 8))
+                    text_number++;
+
+            }
         }
     }
 }
