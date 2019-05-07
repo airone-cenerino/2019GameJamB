4a2ace85ee3dd73ddd4f4bf2d804e6c015b41b7b
nysyo <tds24004@gmail.com>
2019-05-07 12:41:38 +0900

    手紙コライダの変更など

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -7,7 +7,7 @@ namespace Player
     public class PlayerMover : MonoBehaviour
     {
         [SerializeField]
-        float rotateSpeed = 80f;
+        float rotateSpeed = 2f;
         public static float moveSpeed;
         public float playerMoveSpeed;
         private Camera m_Camera;
@@ -34,15 +34,15 @@ namespace Player
         void Update()
         {
 #if UNITY_STANDALONE
-            this.transform.rotation *= Quaternion.Euler(0, rotateSpeed * Input.GetAxis("Mouse X") * Time.deltaTime, 0);
-            m_Camera.transform.rotation *= Quaternion.Euler(-rotateSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime, 0, 0);
+            this.transform.rotation *= Quaternion.Euler(0, rotateSpeed * Input.GetAxis("Mouse X"), 0);
+            m_Camera.transform.rotation *= Quaternion.Euler(-rotateSpeed * Input.GetAxis("Mouse Y"), 0, 0);
 #elif UNITY_WSA
             if(Input.GetKeyDown("joystick button 19")){
                 UnityEngine.XR.InputTracking.Recenter();
             }
             if(Input.GetKey("joystick button 9"))
             {
-                this.transform.rotation *= Quaternion.Euler(0, Time.deltaTime * rotateSpeed * Input.GetAxis("Touch"), 0);
+                this.transform.rotation *= Quaternion.Euler(0, rotateSpeed * Input.GetAxis("Touch"), 0);
             }
 #endif
             forward = new Vector3(m_Camera.transform.forward.x, 0, m_Camera.transform.forward.z).normalized;
